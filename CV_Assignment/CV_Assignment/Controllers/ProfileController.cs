4d894b12dad89268daf9b52f673df6ce16e0e556
Tonmoy10 <tonmoysaha1225@gmail.com>
2022-09-22 16:58:43 +0600

    First push

---
+++ CV_Assignment/CV_Assignment/Controllers/ProfileController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CV_Assignment.Controllers
+{
+    public class ProfileController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Homepage()
+        {
+            return View();
+        }
+
+        public IActionResult Education()
+        {
+            return View();
+        }
+
+        public IActionResult Project()
+        {
+            return View();
+        }
+
+        public IActionResult Reference()
+        {
+            return View();
+        }
+    }
+}
