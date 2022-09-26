cfb9692e56c7e73cb42dcc41029e44c1c486ac5a
Tonmoy10 <tonmoysaha1225@gmail.com>
2022-09-26 21:32:12 +0600

    new push

---
+++ Assignment_mid/Assignment_mid/Controllers/ProfileController.cs
@@ -0,0 +1,33 @@
+﻿using System.Web.Mvc;
+
+namespace Assignment_mid.Controllers
+{
+    public class ProfileController : Controller
+    {
+        // GET: Profile
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Homepage()
+        {
+            return View();
+        }
+
+        public ActionResult Education()
+        {
+            return View();
+        }
+
+        public ActionResult Project()
+        {
+            return View();
+        }
+
+        public ActionResult Reference()
+        {
+            return View();
+        }
+    }
+}
---
+++ Assignment_mid/Assignment_mid/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Assignment_mid.MvcApplication" Language="C#" %>
