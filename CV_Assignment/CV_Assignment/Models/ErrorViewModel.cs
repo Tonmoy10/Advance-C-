9d1991484f4288d32e7cba9e048e6d0344030f60
Tonmoy10 <tonmoysaha1225@gmail.com>
2022-09-26 21:41:09 +0600

    push

--- CV_Assignment/CV_Assignment/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace CV_Assignment.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
